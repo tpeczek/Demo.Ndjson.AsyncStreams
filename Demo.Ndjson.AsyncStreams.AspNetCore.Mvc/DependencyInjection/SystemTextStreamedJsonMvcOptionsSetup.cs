e7b72e7add18c185c44473ea5eb303a7b2ed69e4
Tomasz Pęczek <tpeczek@gmail.com>
2024-09-11 22:45:34 +0200

    Streaming async streamed JSON from desktop with HttpClient (#7)

---
+++ Demo.Ndjson.AsyncStreams.AspNetCore.Mvc/DependencyInjection/SystemTextStreamedJsonMvcOptionsSetup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Demo.Ndjson.AsyncStreams.AspNetCore.Mvc.Formatters;
+
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    internal class SystemTextStreamedJsonMvcOptionsSetup : IConfigureOptions<MvcOptions>
+    {
+        private readonly IOptions<JsonOptions>? _jsonOptions;
+        private readonly ILogger<SystemTextJsonInputFormatter> _inputFormatterLogger;
+
+        public SystemTextStreamedJsonMvcOptionsSetup(IOptions<JsonOptions>? jsonOptions, ILogger<SystemTextJsonInputFormatter> inputFormatterLogger)
+        {
+            _jsonOptions = jsonOptions;
+            _inputFormatterLogger = inputFormatterLogger ?? throw new ArgumentNullException(nameof(inputFormatterLogger));
+        }
+
+        public void Configure(MvcOptions options)
+        {
+            options.InputFormatters.RemoveType<SystemTextJsonInputFormatter>();
+            options.InputFormatters.Add(new SystemTextStreamedJsonInputFormatter(_jsonOptions?.Value, _inputFormatterLogger));
+        }
+    }
+}
---
+++ Demo.Ndjson.AsyncStreams.AspNetCore.Mvc/Formatters/SystemTextStreamedJsonInputFormatter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Logging;
+
+namespace Demo.Ndjson.AsyncStreams.AspNetCore.Mvc.Formatters
+{
+    internal class SystemTextStreamedJsonInputFormatter : SystemTextJsonInputFormatter
+    {
+        private static readonly Type _jsonSerializerType = typeof(JsonSerializer);
+        private static readonly Type _asyncEnumerableType = typeof(IAsyncEnumerable<>);
+        private static readonly string _deserializeAsyncEnumerableMethodName = nameof(JsonSerializer.DeserializeAsyncEnumerable);
+        private static readonly Type[] _deserializeAsyncEnumerableMethodParametersTypes = [typeof(Stream), typeof(JsonSerializerOptions), typeof(CancellationToken)];
+
+        private readonly ConcurrentDictionary<Type, MethodInfo> _deserializeAsyncEnumerableMethods = new();
+
+        public SystemTextStreamedJsonInputFormatter(JsonOptions options, ILogger<SystemTextJsonInputFormatter> logger)
+            : base(options, logger)
+        { }
+
+        public override Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+        {
+            if (context.ModelType.GetGenericTypeDefinition() == _asyncEnumerableType)
+            {
+                MethodInfo deserializeAsyncEnumerableMethod = GetDeserializeAsyncEnumerableMethod(context.ModelType);
+
+                return Task.FromResult(InputFormatterResult.Success(deserializeAsyncEnumerableMethod.Invoke(null, [context.HttpContext.Request.Body, base.SerializerOptions, context.HttpContext.RequestAborted])));
+            }
+
+            return base.ReadRequestBodyAsync(context);
+        }
+
+        private MethodInfo GetDeserializeAsyncEnumerableMethod(Type modelType)
+        {
+            MethodInfo deserializeAsyncEnumerableMethod;
+            if (!_deserializeAsyncEnumerableMethods.TryGetValue(modelType, out deserializeAsyncEnumerableMethod))
+            {
+                deserializeAsyncEnumerableMethod = _jsonSerializerType
+                    .GetMethod(_deserializeAsyncEnumerableMethodName, _deserializeAsyncEnumerableMethodParametersTypes)
+                    .MakeGenericMethod(modelType.GetGenericArguments()[0]);
+
+                _deserializeAsyncEnumerableMethods.TryAdd(modelType, deserializeAsyncEnumerableMethod);
+            }
+
+            return deserializeAsyncEnumerableMethod;
+        }
+    }
+}
